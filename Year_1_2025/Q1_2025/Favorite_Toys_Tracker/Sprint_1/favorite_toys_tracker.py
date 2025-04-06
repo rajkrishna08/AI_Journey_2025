da418f4f3bafb35836ab77483700f913e1bd7d88
Raj Krishna <rajkrishna08@gmail.com>
2025-04-07 01:37:21 +0530

    Day 3, Hour 3: Added toy list management to Favorite Toys Tracker

--- Year_1_2025/Q1_2025/Favorite_Toys_Tracker/Sprint_1/favorite_toys_tracker.py
+++ Year_1_2025/Q1_2025/Favorite_Toys_Tracker/Sprint_1/favorite_toys_tracker.py
@@ -2,20 +2,36 @@
 # Make a box for my name (variable with a string object)
 my_name = "Krishna"  # My Name is Krishna
 
-# Show a welcome message by shouting my name (using the upper() method)
-print("welcome to the krishna favourite store Toy Tracker")
-print("my name is: ", my_name.upper()) # Shout my name
-print ("im ready to keep track of my favourit toys!")
+# Welcome to my magic toy box project!
+print("Welcome to krishna's Favorite Toys Tracker!")
 
+# Tell everyone my name (using a variable and upper() method)
+my_name = "krishna"
+print("My name is:", my_name.upper())
 
-# Favorite Toys Tracker Project - Part 2: Set Up My Toy List
-# Make a box for my toy list (variable with a list object)
-my_toys = ["car", "ball", "doll"]  # A shelf with 3 toys
+# Tell everyone I'm ready (using a string)
+print("I'm ready to keep track of my favourite toys!")
 
-# Show my toy list
-print("Here are my favorite toys:", my_toys)
+# Make a list of my favourite toys (using a list)
+my_toys = ["car", "ball", "doll"]
+print("Here are my favourite toys:", my_toys)
 
-# Count how many toys I have (using len())
+# Count my toys (using len() method)
 print("I have this many toys:", len(my_toys))
 
-print("Here are my favorite toys:", my_toys)
+# Print my toy list again (using a loop)
+print("Here are my favourite toys:")
+for toy in my_toys:
+    print(toy)
+
+# Favorite Toys Tracker Project - Part 3: Manage My Toy List
+# Add a new toy to my list (using append() method)
+my_toys.append("teddy")  # Add a teddy bear
+print("Added a teddy bear! Now my toys are:", my_toys)
+
+# Remove a toy from my list (using remove() method)
+my_toys.remove("ball")  # Take the ball away
+print("Took the ball away! Now my toys are:", my_toys)
+
+# Count my toys again (using len())
+print("I have this many toys now:", len(my_toys))
